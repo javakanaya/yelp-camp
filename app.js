d46d959711f592753e2985a5992c5bfa4f324ebd
Java Kanaya Prada <javakanaya@gmail.com>
2024-01-29 23:58:16 +0700

    validation using JOI

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const ExpressError = require("./utils/ExpressError");
+const { campgroundSchema } = require("./schemas");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
@@ -23,6 +24,18 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+// for validating camprgounds objects
+const validateCampground = (req, res, next) => {
+	// NOT A MONGOOSE SCHEMA, BUT A JOI SCHEMA FOR VALIDATION
+	const { error } = campgroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map((errorList) => errorList.message).join(",");
+		throw new ExpressError(msg, 400);
+	} else {
+		next();
+	}
+};
 
 app.get("/", (req, res) => {
 	res.render("home");
@@ -42,8 +55,10 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
 	"/campgrounds",
+	validateCampground,
 	catchAsync(async (req, res, next) => {
-		if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+		// if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -68,6 +83,7 @@ app.get(
 
 app.put(
 	"/campgrounds/:id",
+	validateCampground,
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findByIdAndUpdate(req.params.id, req.body.campground, { new: true });
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -91,8 +107,8 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-	const { statusCode = 500} = err;
-	if (!err.message) err.message = err.message = 'Oh No, Something WFent Wrong!'
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = err.message = "Oh No, Something WFent Wrong!";
 	res.status(statusCode).render("error", { err });
 });
 
