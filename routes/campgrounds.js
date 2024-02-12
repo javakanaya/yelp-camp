db0efabaa967e40aeef2db392d2a9167537a9e5e
Java Kanaya Prada <javakanaya@gmail.com>
2024-02-12 17:06:34 +0700

    feat: upload images using mutler and cloudinary

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -13,17 +13,28 @@ const campgrounds = require("../controllers/campgrounds");
  */
 const { isLoggedIn, isCampgroundOwner, validateCampground } = require("../middleware");
 
+// for enctype: form/multipart
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
+
 /**
  * ROUTES
  */
 
-router.route("/")
-    .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router
+	.route("/")
+	.get(catchAsync(campgrounds.index))
+	// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+	.post(upload.array("image"), (req, res) => {
+		console.log(req.body, req.files);
+		res.send("IT Worked?!");
+	});
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.route("/:id")
+router
+	.route("/:id")
 	.get(catchAsync(campgrounds.showCampground))
 	.put(isLoggedIn, isCampgroundOwner, validateCampground, catchAsync(campgrounds.updateCampground))
 	.delete(isLoggedIn, isCampgroundOwner, catchAsync(campgrounds.deleteCampground));
