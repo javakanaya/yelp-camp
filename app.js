b17565a2f71cc63ded7df17d6ea197f9b0063c8e
Java Kanaya Prada <javakanaya@gmail.com>
2024-02-13 14:42:18 +0700

    feat: security using helmet

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 const User = require("./models/user");
 
@@ -39,6 +41,64 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+// By default, $ and . characters are removed completely from user-supplied input in the following places:
+// - req.body
+// - req.params
+// - req.headers
+// - req.query
+
+// To remove data using these defaults:
+app.use(
+	mongoSanitize({
+		replaceWith: "_",
+	})
+);
+
+// for manipulating headers for security reasons
+app.use(helmet());
+
+const scriptSrcUrls = [
+	"https://stackpath.bootstrapcdn.com",
+	"https://api.tiles.mapbox.com",
+	"https://api.mapbox.com",
+	"https://kit.fontawesome.com",
+	"https://cdnjs.cloudflare.com",
+	"https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+	"https://kit-free.fontawesome.com",
+	"https://stackpath.bootstrapcdn.com",
+	"https://api.mapbox.com",
+	"https://api.tiles.mapbox.com",
+	"https://fonts.googleapis.com",
+	"https://use.fontawesome.com",
+	"https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = ["https://api.mapbox.com", "https://*.tiles.mapbox.com", "https://events.mapbox.com"];
+const fontSrcUrls = [];
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", "blob:"],
+			childSrc: ["blob:"],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				"blob:",
+				"data:",
+				"https://res.cloudinary.com/drum2urp1/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com",
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+		},
+	})
+);
+
 app.use(
 	session({
 		secret: "thishouldbeabettersecret!",
@@ -46,6 +106,11 @@ app.use(
 		saveUninitialized: true,
 		// options for the cookie
 		cookie: {
+			// changing the default name of 'session.id'
+			name: "session",
+			// only accesabley on https
+			// secure: true,
+			// to set our cookies only accesable through http, not accesible through javascript.
 			httpOnly: true,
 			expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // for a week
 			maxAge: 1000 * 60 * 60 * 24 * 7,
