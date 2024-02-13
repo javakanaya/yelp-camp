c2a6e7b6fe4ad33cca0ce2fda86f6f4ccb1a5713
Java Kanaya Prada <javakanaya@gmail.com>
2024-02-13 11:50:33 +0700

    feat: campgrounds cluster map

--- models/campground.js
+++ models/campground.js
@@ -14,6 +14,9 @@ const imageSchema = new Schema({
 imageSchema.virtual("thumbnail").get(function () {
 	return this.url.replace("/upload/", "/upload/w_200/");
 });
+
+const opts = { toJSON: { virtuals: true } };
+
 const campgroundSchema = new Schema({
 	title: String,
 	images: [imageSchema],
@@ -42,6 +45,13 @@ const campgroundSchema = new Schema({
 			ref: "Review",
 		},
 	],
+}, opts);
+
+campgroundSchema.virtual("properties.popUpMarkup").get(function () {
+	return `
+	<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+	<p>${this.description.substring(0, 20)}...</p>
+	`;
 });
 
 // To delete associated reviews to the campground, the delete route is triggering the "findOneAndDelete"
